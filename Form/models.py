4e5a27ea1d67f6839ad5a1e868770640687e0e53
AltamashBebal <bebalaltamash@gmail.com>
2022-05-29 12:28:40 +0530

    Added form and model

--- Form/models.py
+++ Form/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserData(models.Model):
+    username=models.CharField(max_length = 500)
+    clientname=models.CharField(max_length =500)
+    server=models.CharField(max_length =100)
+    paymentRecieved=models.CharField(max_length =100)
+    paymentType=models.CharField(max_length =100)
+    profit=models.CharField(max_length =100)
+    date=models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.username
+        
--- Form/views.py
+++ Form/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
 # Create your views here.``
-
+from .models import UserData
 
 def index(request):
+    if request.method == 'POST':
+        print(request.POST)
+        data=request.POST
+        server=""
+        if "InternationalServer" in data:
+            server="International Server"
+        if "AsiaServer" in data:
+            server="Asia Server"
+        
+        paymentRecieved=""
+        paymentType=""
+        if "paymentRecieved" in data:
+            paymentRecieved="Payment Recieved"
+        if "OnlinePayment" in data:
+            paymentType="Online Payment"
+        if "OfflinePayment" in data:
+            paymentType="Offline Payment"
+        date=data['date']    
+        username=data['username']
+        clientname=data['clientname']
+        profit=data['profit']
+
+        userdata=UserData(username=username, clientname=clientname, profit=profit,date=date,paymentType=paymentType,paymentRecieved=paymentRecieved,server=server)
+
+        userdata.save()
+        print("model saved")
+        
+
+        
+            
     return render(request,'index.html')
 
 def search(request):
+    userdata =UserData.objects.filter(username="admin1")
+    print(userdata)
     return render(request,'search.html')    
