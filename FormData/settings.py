9b7b2a6464309c3d46a8e6457922ca21c2016342
AltamashBebal <bebalaltamash@gmail.com>
2022-05-29 21:03:07 +0530

    Added RDS Mysql

--- FormData/settings.py
+++ FormData/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'FormData.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'formdata',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': 'qatarlive.cussmttaeeue.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
