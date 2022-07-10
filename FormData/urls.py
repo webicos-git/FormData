462591ee4babbeac6598b260f0afebb7ff16c3cf
AltamashBebal <bebalaltamash@gmail.com>
2022-07-10 09:58:40 +0530

    added login function

--- FormData/urls.py
+++ FormData/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.decorators import login_required
+
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -20,11 +22,13 @@ from Form import views
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name="home"),
     path('search/', views.search),
     path('getPdf/', views.getPdf),
     path('createPdf/', views.createPdf),
-    path('search/delete/<str:id>/', views.deleteUserData,name="deleteUserData"),
-    path('update/<str:id>/', views.update,name="updateUserData"),
-    path('downloadReport/',views.downloadReport,name="downloadReport")
+    path('search/delete/<str:id>/', views.deleteUserData, name="deleteUserData"),
+    path('update/<str:id>/', views.update, name="updateUserData"),
+    path('downloadReport/', views.downloadReport, name="downloadReport"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
